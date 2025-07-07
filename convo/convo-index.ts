7a75b61502c7982136299d1ff851f8b310cc28db
Scott Vance <scott@iyio.io>
2025-07-07 14:31:38 -0400

    - removed old legacy code

--- convo/convo-index.ts
+++
@@ -1,7 +0,0 @@
-export * from './common';
-export * from './convo-types';
-export * from './convo-types-client';
-export * from './convo-types-service';
-export * from './ConvoClient';
-export * from './ConvoServiceMgr';
-
--- openai/convo-openai-index.ts
+++
@@ -1,3 +0,0 @@
-export * from './openai-types';
-export * from './openai-util';
-export * from './OpenAiService';
